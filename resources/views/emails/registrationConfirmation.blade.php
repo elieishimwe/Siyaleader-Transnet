299ff68666be9ffba7f390ccce93d70abbac023b
Elie Ishimwe <elieish@gmail.com>
2015-07-29 08:49:26 +0200

    email template

--- resources/views/emails/registrationConfirmation.blade.php
+++ resources/views/emails/registrationConfirmation.blade.php
@@ -1,3 +1,5 @@
 
 Good Day {{ $name }}. Thank you! for you registration. Please take note of your login credentials:
 Username : {{ $username }}, Password : {{ $password }}.
+
+To login Click http://ports.siyaleader.net/ .
