448f0400b80c72feddd45899d0929b4e830f152c
Elie Ishimwe <elieish@gmail.com>
2015-08-27 16:13:49 +0200

    updates

--- resources/views/cases/refer.blade.php
+++ resources/views/cases/refer.blade.php
@@ -1,10 +1,19 @@
 <!-- Modal Default -->
 <div class="modal modalReferCase" id="modalReferCase" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title" id='depTitle'>Escalate Case</h4>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+
+              </div>
+               <div class="col-md-6">
+                 <a class="btn btn-xs btn-alt" data-toggle="modal" onClick="launchAddressBookModal();" data-target=".modalAddressBook">Address Book</a>
+
+              </div>
             </div>
             <div class="modal-body">
             {!! Form::open(['url' => 'escalateCase', 'method' => 'post', 'class' => 'form-horizontal', 'id'=>"escalateCaseForm" ]) !!}
@@ -12,15 +21,15 @@
 
             <div class="form-group">
                 {!! Form::label('Email Addresses', 'Email Addresses', array('class' => 'col-md-2 control-label')) !!}
-                <div class="col-md-10">
+                <div class="col-md-6">
                   {!! Form::text('addresses',NULL,['class' => 'form-control input-sm','id' => 'addresses']) !!}
                 </div>
             </div>
 
             <div class="form-group">
                 {!! Form::label('Message', 'Message', array('class' => 'col-md-2 control-label')) !!}
-                <div class="col-md-10">
-                    <textarea rows="5" id="message" name="message" class="sms form-control" maxlength="500"></textarea>
+                <div class="col-md-8">
+                    <textarea rows="5" id="message" name="message" class="form-control" maxlength="500"></textarea>
                 </div>
             </div>
 
@@ -32,7 +41,7 @@
 
            <div class="form-group">
                 <div class="col-md-offset-2 col-md-10">
-                    <a class="btn btn-xs btn-alt" data-toggle="modal" onClick="launchAddressBookModal();" data-target=".modalAddressBook">Address Book</a>
+
                 </div>
             </div>
             </div>
