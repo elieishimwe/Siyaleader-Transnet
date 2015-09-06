7646396fe157e9f85d54313f4e3dd737eecdd018
Elie Ishimwe <elieish@gmail.com>
2015-09-06 11:57:22 +0200

    updates

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,27 +1,117 @@
-<!-- resources/views/auth/reset.blade.php -->
 
-<form method="POST" action="{{ url('/password/reset') }}">
-    {!! csrf_field() !!}
-    <input type="hidden" name="token" value="{{ $token }}">
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+<!DOCTYPE html>
+<!--[if IE 9 ]><html class="ie9"><![endif]-->
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <meta name="format-detection" content="telephone=no">
+        <meta charset="UTF-8">
+        <meta name="description" content="Siyaleader Ports Case Console Management">
+        <meta name="keywords" content="Siyaleader, Ports, Trasnet,">
+        <link rel="icon" type="image/x-icon" sizes="16x16" href="/img/favicon.ico?v1">
+
+        <title>Siyaleader</title>
+
+        <!-- CSS -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/form.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/generics.css') }}" rel="stylesheet">
+    </head>
+    <body id="skin-blur-sunset">
+
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+    @endif
+        <section id="login">
+            <header>
+                <h1></h1>
+                <p></p>
+            </header>
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+            <div class="clearfix"></div>
 
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
+            <!-- resources/views/auth/reset.blade.php -->
 
-    <div>
-        <button type="submit">
-            Reset Password
-        </button>
-    </div>
-</form>
+            <form method="POST" action="{{ url('/password/reset') }}">
+                {!! csrf_field() !!}
+                <input type="hidden" name="token" value="{{ $token }}">
+
+                <div>
+                    Email
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+
+                <div>
+                    Password
+                    <input type="password" name="password">
+                </div>
+
+                <div>
+                    Confirm Password
+                    <input type="password" name="password_confirmation">
+                </div>
+
+                <div>
+                    <button type="submit">
+                        Reset Password
+                    </button>
+                </div>
+            </form>
+
+        </section>
+
+        <!-- Javascript Libraries -->
+        <!-- jQuery -->
+        <script src="{{ asset('js/jquery.min.js') }}"></script> <!-- jQuery Library -->
+
+        <!-- Bootstrap -->
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+        <!--  Form Related -->
+        <script src="{{ asset('js/icheck.js') }}"></script> <!-- Custom Checkbox + Radio -->
+
+        <!-- All JS functions -->
+        <script src="{{ asset('js/functions.js') }}"></script>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
