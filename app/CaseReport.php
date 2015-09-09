777a557ae0e8a7a5fcf28f5baf8ab137098a8df9
Elie Ishimwe <elieish@gmail.com>
2015-09-09 14:32:37 +0200

    updates

--- app/CaseReport.php
+++ app/CaseReport.php
@@ -7,7 +7,7 @@ class CaseReport extends Eloquent
 
 
     protected $table    = 'cases';
-    protected $fillable = ['description','user','department','category','sub_category','sub_sub_category','priority','status','gps_lat','gps_lng','img_url','active'];
+    protected $fillable = ['description','user','department','category','sub_category','sub_sub_category','priority','status','gps_lat','gps_lng','img_url','active','severity'];
 
 
 
