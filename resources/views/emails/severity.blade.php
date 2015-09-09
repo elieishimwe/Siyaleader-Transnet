33d4d205420c5d0af24682d5a9a426b02f424837
Elie Ishimwe <elieish@gmail.com>
2015-09-09 15:19:55 +0200

    updates

--- resources/views/emails/severity.blade.php
+++ resources/views/emails/severity.blade.php
@@ -1,1 +1,1 @@
-IL{{ $severity }}. Reporter: {{ $name }}. Cell: {{ $cell }}. Category {{ $category }}. REF: {{ $caseId }} . http://ports.siyaleader.net;
+IL{{ $severity }} alert. Reporter: {{ $name }}. Cell: {{ $cell }}. Category {{ $category }}. REF: {{ $caseId }}. http://ports.siyaleader.net
