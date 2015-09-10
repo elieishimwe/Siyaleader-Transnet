0597afd684e71d18dd84850106afe0ee292120f7
Elie Ishimwe <elieish@gmail.com>
2015-09-10 09:15:09 +0200

    update

--- resources/views/emails/severity.blade.php
+++ resources/views/emails/severity.blade.php
@@ -1,1 +1,1 @@
-Level {{ $severity }} alert. Reporter: {{ $name }}. Cell: {{ $cell }}. Category {{ $category }}. REF: {{ $caseId }}. at Durban. http://ports.siyaleader.net
+LEVEL {{ $severity }} alert. Reporter: {{ $name }}. Cell: {{ $cell }}. Category {{ $category }}. REF: {{ $caseId }}. at Durban. http://ports.siyaleader.net
