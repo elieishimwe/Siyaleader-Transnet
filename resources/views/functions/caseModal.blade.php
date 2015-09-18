f99751258fcd20394d432e3432f9de0c2724a5da
Elie Ishimwe <elieish@gmail.com>
2015-09-18 10:56:07 +0200

    updates

--- resources/views/functions/caseModal.blade.php
+++ resources/views/functions/caseModal.blade.php
@@ -234,7 +234,7 @@
         success : function(data){
 
           if (data == 'ok') {
-            $(".token-input-list").remove();
+            $(".token-input-token").remove();
             $('#escalateCaseForm')[0].reset();
             $("#caseNotesNotification").html('<div class="alert alert-success alert-icon">Well done! You case has been successfully escalated <i class="icon">&#61845;</i><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>');
             launchCaseModal(caseID);
