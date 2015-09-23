b1db2b302637f9c31a0a6238854ae431fef6ca01
Elie Ishimwe <elieish@gmail.com>
2015-09-23 13:33:30 +0200

    updates

--- database/migrations/2015_09_09_122636_addColumnsCaseTable.php
+++ database/migrations/2015_09_09_122636_addColumnsCaseTable.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsCaseTable extends Migration
+class AddColumnsCasessTable extends Migration
 {
     /**
      * Run the migrations.
