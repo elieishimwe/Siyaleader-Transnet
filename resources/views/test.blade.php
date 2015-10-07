cad01f055035fa3092ea445bd26dd7e8fbcd26eb
Elie Ishimwe <elieish@gmail.com>
2015-10-07 13:39:02 +0200

    updates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,11 +9,10 @@
     <script src="{{ asset('js/socket.io.js') }}"></script>
 
     <script>
-    var socket = io('http://127.0.0.1:6379');
+    var socket = io('http://localhost:3000');
     socket.on("test-channel:App\\Events\\MyEventNameHere", function(message){
-        console.log("elie");
-        console.log(message);
-        $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
+         // increase the power everytime we load test route
+         $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
      });
     </script>
 
