b6c8c9a5c79deb23214406233ee2a66f75d8ba22
Elie Ishimwe <elieish@gmail.com>
2015-10-07 14:58:29 +0200

    updates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,8 @@
     <script src="{{ asset('js/socket.io.js') }}"></script>
 
     <script>
-    var socket = io('http://localhost:3000');
+  /*  var socket = io('http://localhost:3000');*/
+  /*  var socket = io('http://41.216.130.6:3000');*/
     socket.on("test-channel:App\\Events\\MyEventNameHere", function(message){
          // increase the power everytime we load test route
          console.log(message);
