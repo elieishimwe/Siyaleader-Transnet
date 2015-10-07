71c6bd1767fd150734a43347d09f4f6479105c64
Elie Ishimwe <elieish@gmail.com>
2015-10-07 13:24:45 +0200

    updates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -11,8 +11,8 @@
     <script>
     var socket = io('http://127.0.0.1:6379');
     socket.on("test-channel:App\\Events\\MyEventNameHere", function(message){
-        alert(elie);
-        alert(message);
+        console.log("elie");
+        console.log(message);
         $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
      });
     </script>
