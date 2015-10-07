8ed2a3a5e91bb218933efabf00f70f1edc65be30
Elie Ishimwe <elieish@gmail.com>
2015-10-07 13:15:59 +0200

    updates

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,7 @@
     <script src="{{ asset('js/socket.io.js') }}"></script>
 
     <script>
-    var socket = io('http://localhost:3000');
+    var socket = io('http://localhost:6379');
     socket.on("test-channel:App\\Events\\MyEventNameHere", function(message){
          $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
      });
